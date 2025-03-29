cba92dafe0ca046a19a50a6be733a761a7c8ccad
Evan <evanboisdanghien@gmail.com>
2025-03-30 00:36:35 +0100

    Commit Final

--- src/header/header.js
+++ src/header/header.js
@@ -1,8 +1,12 @@
 import header from './header.css';
+
 export default function Header() {
     return (
         <header className="header">
             <div className="header-container">
+                <div className="logo">
+                    <i className="fa fa-calendar-o" aria-hidden="true"></i>
+                </div>
                 <h1 className="header-title">To-Do List</h1>
             </div>
         </header>
